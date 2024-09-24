60dc172b63249163c3d71c5537a0b5cd7fba2fe3
Charles Woods <charles.woods48@yahoo.com>
2024-09-24 15:07:28 -0500

    broken image, fix next session

--- ctcwd/src/pages/Home.jsx
+++ ctcwd/src/pages/Home.jsx
@@ -1,10 +1,28 @@
-
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 export default function Home() {
     return (
       <>
-      <div>
-        
+      <div id='main-home'>
+
+      <div id="left-column-components">
+        <div id="company-name">
+          <h1>Coast to Coast web design</h1>
+        </div>
+
+        <div id="button-container">
+        <Button variant="text">Meet the Team</Button>
+        <Button variant="text">Portfolio</Button>
+        </div>
+      </div>
+
+      <div id="right-column-components">
+        <div id="group-image">
+          <img src="../../public/images/groupPhoto.png" alt="group working" />
+        </div>
+        <Button variant="text">Work with us</Button>
+      </div>
       </div>
       </>
     )
