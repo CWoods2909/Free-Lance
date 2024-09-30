9c9afd62b92ea1dca9dfdbce2162523486cc9362
Yooboo Park <91972819+arihu1008@users.noreply.github.com>
2024-09-30 15:25:57 -0500

    Added css on home page

--- ctcwd/src/pages/Home.jsx
+++ ctcwd/src/pages/Home.jsx
@@ -1,28 +1,29 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import './Home.css';
 
 export default function Home() {
     return (
       <>
       <div id='main-home'>
 
-      <div id="left-column-components">
-        <div id="company-name">
-          <h1>Coast to Coast web design</h1>
+        <div id="left-column-components">
+          <div id="company-name">
+            <h1>Coast to Coast<br></br> web design</h1>
+          </div>
+
+          <div id="button-container">
+          <Button variant="text">Meet the Team</Button>
+          <Button variant="text">Portfolio</Button>
+          </div>
         </div>
 
-        <div id="button-container">
-        <Button variant="text">Meet the Team</Button>
-        <Button variant="text">Portfolio</Button>
+        <div id="right-column-components">
+          <div id="group-image">
+            <img src="/images/groupPhoto.png" alt="group working" style={{height: "60vh", paddingRight: "5%" }}/>
+          </div>
+          <Button variant="text">Work with us</Button>
         </div>
-      </div>
-
-      <div id="right-column-components">
-        <div id="group-image">
-          <img src="../../public/images/groupPhoto.png" alt="group working" />
-        </div>
-        <Button variant="text">Work with us</Button>
-      </div>
       </div>
       </>
     )
