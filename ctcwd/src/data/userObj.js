a76f1c4d6f443f7f7bb04e4903f4873697b5ae1c
Yooboo Park <91972819+arihu1008@users.noreply.github.com>
2024-10-04 09:49:38 -0500

    profile pictures swapped

--- ctcwd/src/data/userObj.js
+++ ctcwd/src/data/userObj.js
@@ -4,20 +4,20 @@ export const user = [
         lastName: "Woods",
         image: "/images/charlesPic.jpg",
         description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
-        printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+        printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining essentially unchanged. `,
         linkedinUrl: "https://www.linkedin.com/in/charles-woods-319a83231/",
         gitHubUrl: "https://github.com/CWoods2909",
         email: "blank"
     },
     {
-        firstName: "Yooboo",
+        firstName: "Yooboo Arihu",
         lastName: "Park",
-        image: "/images/YoobooPic.jpg",
+        image: "/images/yoobooPicture.jpeg",
         description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
-        printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+        printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
         linkedinUrl: "https://www.linkedin.com/in/yooboopark/",
         gitHubUrl: "https://github.com/arihu1008",
@@ -28,8 +28,8 @@ export const user = [
         lastName: "Soca",
         image: "/images/andresPic.jpg",
         description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
-        printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+        printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
         linkedinUrl: "https://www.linkedin.com/in/andres-soca-23852aab/",
         gitHubUrl: "https://github.com/DevLumen",
